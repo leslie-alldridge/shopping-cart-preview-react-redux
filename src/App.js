37a5178177beec5de5eadcc50d4da15adc7e859c
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-17 19:57:32 +1300

    set up hello world grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid } from 'react-bootstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <Grid className="purchase-card">
+          <h1>Hello world</h1>
+        </Grid>
       </div>
     );
   }
