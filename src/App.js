716aa1d4c2fa509353ada8113d5a502f940fce27
leslie-alldridge <leslie.alldridge@gmail.com>
2018-10-17 20:11:32 +1300

    tooltip savings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
+
+import Subtotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 100,
+      pickupSavings: -3.85
+    };
+  }
+
   render() {
     return (
       <div className="container">
         <Grid className="purchase-card">
-          <h1>Hello world</h1>
+          <Subtotal price={this.state.total.toFixed(2)} />
+          <PickupSavings price={this.state.pickupSavings} />
         </Grid>
       </div>
     );
